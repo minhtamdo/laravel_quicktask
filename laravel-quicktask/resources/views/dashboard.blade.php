9d74befbf513d8fa6631d3af01b2f87274238b7a
minhtamdo <tammdo2004@gmail.com>
2025-08-04 15:07:43 +0700

    lesson 10 bower laravel mix

--- laravel-quicktask/resources/views/dashboard.blade.php
+++ laravel-quicktask/resources/views/dashboard.blade.php
@@ -10,8 +10,21 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
+
+                    <button id="showMessage" class="mt-4 px-4 py-2 bg-blue-500 text-white rounded">Click me</button>
+                    <div id="message" class="mt-2 text-green-500 hidden">Hello from jQuery!</div>
                 </div>
             </div>
         </div>
     </div>
+
+    {{-- jQuery script --}}
+    <script src="{{ asset('bower_components/jquery/dist/jquery.min.js') }}"></script>
+    <script>
+        $(document).ready(function() {
+            $('#showMessage').click(function() {
+                $('#message').fadeIn().delay(2000).fadeOut();
+            });
+        });
+    </script>
 </x-app-layout>
